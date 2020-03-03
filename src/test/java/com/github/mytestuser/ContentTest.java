6c585bb348a565e9fafb24fbb701a52827b8d39f
Weronika Lisay <48102128+weronikalisay7@users.noreply.github.com>
2020-03-03 16:47:36 +0300

    Add files via upload

---
+++ src/test/java/com/github/mytestuser/ContentTest.java
@@ -0,0 +1,66 @@
+package com.github.mytestuser;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+public class ContentTest {
+    @BeforeEach
+
+    public void setUp(){
+        Configuration.headless=true;
+        open("http://automationpractice.com/index.php");
+    }
+    @Test
+    public void saleBannerClick(){
+        $(By.xpath("//*[@id='htmlcontent_top']/ul/li[1]/a/img")).click();
+    }
+    @Test
+    public void summerCollectionBannerClick(){
+        $(By.xpath("//*[@id='htmlcontent_top']/ul/li[2]/a/img")).click();
+    }
+    @Test
+    public void topTrendsClick(){
+        $(By.xpath("//*[@id='htmlcontent_home']/ul/li[1]/a/img")).click();
+    }
+    @Test
+    public void mensCoatsJacketsClick(){
+        $(By.xpath("//*[@id='htmlcontent_home']/ul/li[2]/a/img")).click();
+    }
+    @Test
+    public void womenCoatsJacketsClick(){
+        $(By.xpath("//*[@id='htmlcontent_home']/ul/li[3]/a/img")).click();
+    }
+    @Test
+    public void sunglassesEyewearClick(){
+        $(By.xpath("//*[@id='htmlcontent_home']/ul/li[4]/a/img")).click();
+    }
+    @Test
+    public void hadbagsClick(){
+        $(By.xpath("//*[@id='htmlcontent_home']/ul/li[5]/a/img")).click();
+    }
+    /*@Test
+    public void eyeIconClick(){
+        $(By.xpath("//*[@id='homefeatured']/li[1]/div/div[1]/div/div[1]/a/i")).click();
+    }*/
+    @Test
+    public void clickByItemName(){
+        $(By.xpath("//*[@id='homefeatured']/li[1]/div/div[2]/h5/a")).click();
+    }
+    @Test
+    public void clickByItemImg(){
+        $(By.xpath("//*[@id='homefeatured']/li[1]/div/div[1]/div/a[1]/img")).click();
+    }
+    /*@Test
+    public void addToCartClick(){
+        $(By.xpath("//*[@id='homefeatured']/li[1]/div/div[2]/div[2]/a[1]/span")).click();
+    }
+    @Test
+    public void moreClick(){
+        $(By.xpath("//*[@id='homefeatured']/li[1]/div/div[2]/div[2]/a[2]/span")).click();
+    }*/
+
+}
